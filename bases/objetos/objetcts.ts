6106f1454ca00b4e70c6622373a6ccce5048b0ff
leedd87 <leedav87@gmail.com>
2022-08-16 23:36:16 -0300

    objetos en tsc

---
+++ bases/objetos/objetcts.ts
@@ -0,0 +1,33 @@
+(() => {
+
+   let flash: { name: string, age?: number, powers: string[], getName?: () => string } = {
+      name: 'Barry Allen',
+      age: 24,
+      powers: ['Super velocidad', 'Viajar en el tiempo']
+
+   }
+
+   let superman: { name: string, age?: number, powers: string[], getName?: () => string } = {
+      name: 'Clark Kent',
+      age: 60,
+      powers: ['Super velocidad']
+
+   }
+
+
+
+   // flash = {//typescript no deja agregar propiedades a los objetos.
+   //    name: 'Clark Kent',
+   //    // age: 60,
+   //    powers: ['Super Fuerza'],
+   //    getName() {
+   //       return this.name
+   //    }
+   // }
+
+   // console.log(flash).
+
+
+
+
+})()
