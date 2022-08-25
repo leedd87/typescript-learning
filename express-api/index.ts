47cf9f8537ef92b5d3a0c4bdb1829b8e0d1a5c14
leedd87 <leedav87@gmail.com>
2022-08-24 22:45:08 -0300

    express tsc

---
+++ express-api/index.ts
@@ -0,0 +1,26 @@
+import express from 'express'
+//const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+
+	res.status(401).json({
+		ok: false,
+		msg: 'No hay token en la peticion'
+	})
+
+	res.json({
+		ok: true,
+		msg: 'Todo salio bien!'
+	})
+
+
+});
+
+app.listen(port, () => {
+	console.log(
+		`Ejemplo de la app escuchando en el puerto http://localhost:${port}`
+	);
+});
